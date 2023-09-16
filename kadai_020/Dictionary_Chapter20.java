4af565c75fbd31bfdcf1018818f82ef8a34ed0db
AikoSudo <144648290+AikoSudo@users.noreply.github.com>
2023-09-17 00:57:43 +0900

    for文の修正

--- kadai_020/Dictionary_Chapter20.java
+++ kadai_020/Dictionary_Chapter20.java
@@ -4,30 +4,34 @@ import java.util.HashMap;
 
 public class Dictionary_Chapter20 {
 	// HashMapの宣言
-	HashMap<String,String> fruits; 
+	HashMap<String, String> fruits;
+
 	// コンストラクタ
 	public Dictionary_Chapter20() {
-		fruits = new HashMap<String,String>();
-	
-	// 要素を追加
-	fruits.put("apple", "りんご");
-	fruits.put("peach", "桃");
-	fruits.put("banana", "バナナ");
-	fruits.put("lemon", "レモン");
-	fruits.put("pear", "梨");
-	fruits.put("kiwi", "キウィ");
-	fruits.put("strawberry", "いちご");
-	fruits.put("grape", "ぶどう");
-	fruits.put("muscat", "マスカット");
-	fruits.put("cherry", "さくらんぼ");
+		fruits = new HashMap<String, String>();
+
+		// 要素を追加
+		fruits.put("apple", "りんご");
+		fruits.put("peach", "桃");
+		fruits.put("banana", "バナナ");
+		fruits.put("lemon", "レモン");
+		fruits.put("pear", "梨");
+		fruits.put("kiwi", "キウィ");
+		fruits.put("strawberry", "いちご");
+		fruits.put("grape", "ぶどう");
+		fruits.put("muscat", "マスカット");
+		fruits.put("cherry", "さくらんぼ");
 	}
-	
+
 	public void referer(String[] words) {
-		for(String setWords: words) {
-			if(setWords == "apple" || setWords == "banana" || setWords == "grape") {
-				System.out.println(setWords + "の意味は" + fruits.get(setWords));
+		for (String word : words) {
+			// 配列のキーがMap内に存在するか調べる
+			String result = fruits.get(word);
+			//辞書を調べた結果により出力を分岐
+			if (result == null) {
+				System.out.println(word + "は辞書に存在しません");
 			} else {
-				System.out.println(setWords + "は辞書に存在しません");
+				System.out.println(word + "の意味は" + result);
 			}
 		}
 	}
