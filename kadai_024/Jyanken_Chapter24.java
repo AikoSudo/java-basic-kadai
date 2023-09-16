d14650e4bdb71a8bdb097edea36e42b00da6982f
AikoSudo <144648290+AikoSudo@users.noreply.github.com>
2023-09-17 01:08:36 +0900

    比較結果を修正

--- kadai_024/Jyanken_Chapter24.java
+++ kadai_024/Jyanken_Chapter24.java
@@ -6,56 +6,51 @@ import java.util.Scanner;
 public class Jyanken_Chapter24 {
 	public String getMyChoice() {
 		Scanner scan = new Scanner(System.in);
-		
-		while(true) {
+
+		while (true) {
 			System.out.println("自分のじゃんけんの手を入力しましょう");
 			System.out.println("グーはrockのrを入力しましょう");
 			System.out.println("チョキはscissorsのsを入力しましょう");
 			System.out.println("パーはpaperのpを入力しましょう");
-			
+
 			String input = scan.nextLine();
-			
+
 			// r,s,pが入力された場合
-			if(input.equals("r") || input.equals("s") || input.equals("p")) {
+			if (input.equals("r") || input.equals("s") || input.equals("p")) {
+				scan.close();
 				return input;
 			} else {
 				System.out.println("正しいじゃんけんの手を入力してください");
 			}
 		}
 	}
-	
+
 	public String getRandom() {
-		String[] hands = {"r", "s", "p"};
-		int random = (int)Math.floor(Math.random() * 3);
+		String[] hands = { "r", "s", "p" };
+		int random = (int) Math.floor(Math.random() * 3);
 		return hands[random];
 	}
-	
+
 	public void playGame(String myChoice, String random) {
 		HashMap<String, String> jyankenKeep = new HashMap<String, String>();
 		jyankenKeep.put("r", "グー");
 		jyankenKeep.put("s", "チョキ");
 		jyankenKeep.put("p", "パー");
-		
+
 		System.out.println("自分の手は" + jyankenKeep.get(myChoice) + "、" + "対戦相手の手は" + jyankenKeep.get(random));
-		
-		if(jyankenKeep.get("r") == jyankenKeep.get(myChoice) && jyankenKeep.get("r") == jyankenKeep.get(random)) {
-			System.out.println("あいこです");
-		} else if(jyankenKeep.get("r") == jyankenKeep.get(myChoice) && jyankenKeep.get("s") == jyankenKeep.get(random)) {
-			System.out.println("自分の勝ちです");
-		} else if(jyankenKeep.get("r") == jyankenKeep.get(myChoice) && jyankenKeep.get("p") == jyankenKeep.get(random)) {
-			System.out.println("自分の負けです");
-		} else if(jyankenKeep.get("s") == jyankenKeep.get(myChoice) && jyankenKeep.get("r") == jyankenKeep.get(random)) {
-			System.out.println("自分の負けです");
-		} else if(jyankenKeep.get("s") == jyankenKeep.get(myChoice) && jyankenKeep.get("s") == jyankenKeep.get(random)) {
-			System.out.println("あいこです");
-		} else if(jyankenKeep.get("s") == jyankenKeep.get(myChoice) && jyankenKeep.get("p") == jyankenKeep.get(random)) {
-			System.out.println("自分の勝ちです");
-		} else if(jyankenKeep.get("p") == jyankenKeep.get(myChoice) && jyankenKeep.get("r") == jyankenKeep.get(random)) {
-			System.out.println("自分の勝ちです");
-		} else if(jyankenKeep.get("p") == jyankenKeep.get(myChoice) && jyankenKeep.get("s") == jyankenKeep.get(random)) {
-			System.out.println("自分の負けです");
-		} else {
-			System.out.println("あいこです");
+
+		String result = "";
+
+		if (myChoice.equals("r") && random.equals("s") || myChoice.equals("s") && random.equals("p")
+				|| myChoice.equals("p") && random.equals("r")) { // 自分が勝つ場合
+			result = "自分の勝ちです";
+		} else if (myChoice.equals("r") && random.equals("p") || myChoice.equals("s") && random.equals("r")
+				|| myChoice.equals("p") && random.equals("s")) { // 自分が負ける場合
+			result = "自分の負けです";
+		} else { // あいこの場合
+			result = "あいこです";
 		}
+
+		System.out.println(result);
 	}
 }
